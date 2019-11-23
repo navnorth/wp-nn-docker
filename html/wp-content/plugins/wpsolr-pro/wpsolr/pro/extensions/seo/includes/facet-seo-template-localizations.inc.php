f3b2c2f114b996a294370a61bb60362ed7b76af7
joe hobson <joe@navigationnorth.com>
2019-11-23 12:48:35 -0600

    remove wpsolr pro

--- html/wp-content/plugins/wpsolr-pro/wpsolr/pro/extensions/seo/includes/facet-seo-template-localizations.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$facet_item_seo_template = ( ! empty( $selected_facets_seo_items_templates[ $selected_val ] ) && ! empty( $selected_facets_seo_items_templates[ $selected_val ][ $facet_item_label ] ) )
-	? $selected_facets_seo_items_templates[ $selected_val ][ $facet_item_label ] : '';
-
-include 'facet-seo-template-localizations-field.inc.php';
-//include 'facet-seo-template-range.inc.php';
-?>
